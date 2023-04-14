b659f41941f51e0f400f6aac8d3f58761749a9f0
Diego <diego.herrerat@usil.pe>
2023-04-14 11:25:17 -0500

    update

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -1,9 +1,11 @@
 package com.devsecops;
 
 
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -11,29 +13,44 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 //import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@Autowired
+public class NumericApplicationTests {
 
-class NumericApplicationTests {
-
-    private Object mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void smallerThanOrEqualToFiftyMessage() throws Exception {
-        ((MockMvc) this.mockMvc).perform(get("/compare/50")).andDo(print()).andExpect(status().isOk())
+    public void smallerThanOrEqualToFiftyMessage() throws Exception {
+        this.mockMvc.perform(get("/compare/50")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Smaller than or equal to 50"));
     }
 
     @Test
-    void greaterThanFiftyMessage() throws Exception {
-        ((MockMvc) this.mockMvc).perform(get("/compare/51")).andDo(print()).andExpect(status().isOk())
+    public void greaterThanFiftyMessage() throws Exception {
+        this.mockMvc.perform(get("/compare/51")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Greater than 50"));
-
-
     }
+    
+  @Test
+    public void welcomeMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string("Kubernetes DevSecOps"));
+    }
+    
 
 }
