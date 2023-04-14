99c0ece4ac0c35d88ce975410923c492f4a3be04
Diego <diego.herrerat@usil.pe>
2023-04-13 23:20:21 -0500

    update7

--- zap.sh
+++ zap.sh
@@ -5,7 +5,7 @@ PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].node
 # first run this
 chmod 777 $(pwd)
 echo $(id -u):$(id -g)
-sudo docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+sudo docker run -v $(pwd)/wrk:/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
 
 exit_code=$?
 
