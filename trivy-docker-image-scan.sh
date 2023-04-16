deac3c446a121bc6750c20b6de07e6f2490b1283
Diego <diego.herrerat@usil.pe>
2023-04-15 23:41:43 -0500

    tryvy 1

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -3,8 +3,9 @@
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.18.3 -q image --exit-code 0 --severity HIGH --light $dockerImageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.18.3 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.18.3 -q image --exit-code 0 --severity HIGH --light openjdk:8-jdk-alpine
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.18.3 -q image --exit-code 1 --severity CRITICAL --light openjdk:8-jdk-alpine
+
 
     # Trivy scan result processing
     exit_code=$?
