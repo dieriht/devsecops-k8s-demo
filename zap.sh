61ea4f59818f1c951bed02cc67422d254c71657a
Diego <diego.herrerat@usil.pe>
2023-04-15 21:11:22 -0500

    zap11

--- zap.sh
+++ zap.sh
@@ -6,24 +6,10 @@ PORT=$(kubectl get services devsecops-svc -o json | jq .spec.ports[].nodePort)
 # first run this
 chmod 777 $(pwd)
 echo $(id -u):$(id -g)
-#docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $IP:$PORT -f openapi -r zap_report.html
-#sudo docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT -f openapi -r zap_report.html
 
-exit_code=$?
-
-# comment above cmd and uncomment below lines to run with CUSTOM RULES
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT -f openapi -c zap-rules -w report.md -J json_report.json -r zap_report.html
+sudo docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT -f openapi -r zap_report.html
 
 # HTML Report
  sudo mkdir -p owasp-zap-report
  sudo mv zap_report.html owasp-zap-report
 
-
-echo "Exit Code : $exit_code"
-
- if [[ ${exit_code} -ne 0 ]];  then
-    echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
-    exit 1;
-   else
-    echo "OWASP ZAP did not report any Risk"
- fi;
