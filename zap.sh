ee3c169ec8a63aba95f34cdacef7d0bab6af1d6c
Diego <diego.herrerat@usil.pe>
2023-04-15 20:47:25 -0500

    zap10

--- zap.sh
+++ zap.sh
@@ -7,12 +7,12 @@ PORT=$(kubectl get services devsecops-svc -o json | jq .spec.ports[].nodePort)
 chmod 777 $(pwd)
 echo $(id -u):$(id -g)
 #docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $IP:$PORT -f openapi -r zap_report.html
-sudo docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT -f openapi -r zap_report.html
+#sudo docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT -f openapi -r zap_report.html
 
 exit_code=$?
 
 # comment above cmd and uncomment below lines to run with CUSTOM RULES
-# docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap-rules -w report.md -J json_report.json -r zap_report.html
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT -f openapi -c zap-rules -w report.md -J json_report.json -r zap_report.html
 
 # HTML Report
  sudo mkdir -p owasp-zap-report
