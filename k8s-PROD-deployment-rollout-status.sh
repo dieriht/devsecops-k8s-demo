3ffb7e8a5bc0ee2113df4aeed3ba8156c900e6d6
Diego <diego.herrerat@usil.pe>
2023-05-09 18:35:27 -0500

    upfate

--- k8s-PROD-deployment-rollout-status.sh
+++ k8s-PROD-deployment-rollout-status.sh
@@ -5,7 +5,7 @@ if [[ $(kubectl -n prod rollout status deploy ${deploymentName} --timeout 5s) !=
 then     
 	echo "Deployment ${deploymentName} Rollout has Failed"
     kubectl -n prod rollout undo deploy ${deploymentName}
-    exit 1;
+    #exit 1;
 else
 	echo "Deployment ${deploymentName} Rollout is Success"
 fi
